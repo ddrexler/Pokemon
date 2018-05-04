adc93eb7561cc2b520abd79ae6e26b0cdda09e14
David Drexler <david.drexler.dd@gmail.com>
2018-05-04 16:49:58 +0200

    Simple 'Pokemon' game, for exercise purposes

---
+++ Pokemon/Pokemon.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pokemon
+{
+    class Pokemon
+    {
+        public string Typ { get; set; }
+
+        //false = männlich, true = weiblich
+        public bool Geschlecht { get; set; }
+
+        public string Name { get; set; }
+
+        public int Nummer { get; set; }
+
+        public int Gewicht { get; set; }
+
+        public int Geschwindigkeit { get; set; }
+
+        public int Level { get; set; }
+
+        public int Gesundheit { get; set; }
+
+
+
+        //Methoden
+        public int Tackle()
+        {
+            return (Gewicht + Geschwindigkeit) * Level;
+        }
+
+
+    }
+}
---
+++ Pokemon/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Pokemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pokemon pikachu = new Pokemon
+            {
+                Geschlecht = false,
+                Gewicht = 6,
+                Level = 5,
+                Geschwindigkeit = 85,
+                Name = "Pikachu",
+                Nummer = 25
+            };
+
+            Pokemon schiggy = new Pokemon
+            {
+                Geschlecht = false,
+                Gewicht = 9,
+                Level = 6,
+                Geschwindigkeit = 40,
+                Name = "Schiggy",
+                Nummer = 7
+            };
+
+            Console.WriteLine("{0} greift mit Tackle an! Schaden: {1}", pikachu.Name, pikachu.Tackle());
+            Console.WriteLine("{0} greift mit Tackle an! Schaden: {1}", schiggy.Name, schiggy.Tackle());
+
+            Console.ReadLine();
+        }
+    }
+}
